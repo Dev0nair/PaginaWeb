d29ff5bb0d6fe05399e2e148f2dd0e3fbc6c95c0
Ismael Gonzalez Risco <40613999+Dev0nair@users.noreply.github.com>
2019-01-03 17:23:12 +0100

    Uso de log
    Se añade el uso de console.log donde mostrará lo que hace la web con las busquedas de las peliculas.

--- js.js
+++ js.js
@@ -6,13 +6,17 @@ function quitarPeliculas(){
 	var numP = document.getElementsByClassName(tipo).length;
 	
 	if (numP == 0) {
+		console.log("No se ha encontrado ningún resultado por lo que se activaran todas las peliculas.")
 		ponerTodos();
 	} else {
+		console.log("Se va a mostrar las peliculas de tipo " , tipo);
 		for (var i = 0; i < peliculas.length; i++) {	
 			if (peliculas[i].className != tipo) {
 				peliculas[i].style.display = "none";
+				console.log("La pelicula " , peliculas[i].id, " se desactiva");
 			} else {
 				peliculas[i].style.display = "block";
+				console.log("La pelicula " , peliculas[i].id, " se activa");
 			}
 		}
 	}
